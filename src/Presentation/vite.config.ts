c2a764c359e559fbbae4eb0bd7362992ab79379f
Pillburn <lilburnpeter@gmail.com>
2026-04-06 17:21:47 +0100

    updates to package.json and vite.config

--- src/Presentation/vite.config.ts
+++ src/Presentation/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import fs from 'fs'
-import path from 'path'
+import fs from 'node:fs'
+import path from 'node:path'
 
 export default defineConfig({
   plugins: [react()],
